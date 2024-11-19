c24fbeccd46ed9837d37783e925e719f713efc4d
prashamsha7 <107231728+prashamsha7@users.noreply.github.com>
2024-11-19 18:58:19 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 5, secondNumber = 25;
+
+            Console.WriteLine("Before swapping:");
+            Console.WriteLine("firstNumber = " + firstNumber);
+            Console.WriteLine("secondNumber = " + secondNumber);
+
+            // Swap using the XOR trick
+            firstNumber ^= secondNumber;
+            secondNumber ^= firstNumber;
+            firstNumber ^= secondNumber;
+
+            Console.WriteLine("\nAfter swapping:");
+            Console.WriteLine("firstNumber = " + firstNumber);
+            Console.WriteLine("secondNumber = " + secondNumber);
+        }
+    }
+}
